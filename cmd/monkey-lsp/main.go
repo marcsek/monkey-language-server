a77f65122be08173f35446fad2cfb5674aab88e2
marcsek <jakubmarcek955@gmail.com>
2024-07-04 20:51:13 +0200

    cleanup

--- cmd/monkey-lsp/main.go
+++ cmd/monkey-lsp/main.go
@@ -2,13 +2,11 @@ package main
 
 import (
 	"bufio"
-	"encoding/json"
-	"io"
 	"log"
 	"os"
 
 	"github.com/marcsek/monkey-language-server/internal/analysis"
-	"github.com/marcsek/monkey-language-server/internal/lsp"
+	"github.com/marcsek/monkey-language-server/internal/messageHandler"
 	"github.com/marcsek/monkey-language-server/internal/rpc"
 )
 
@@ -18,11 +16,15 @@ func main() {
 	logger := getLogger(LOG_FILE_PATH)
 	logger.Println("Logger started!")
 
-	scanner := bufio.NewScanner(os.Stdin)
+	reader := os.Stdin
+	writer := os.Stdout
+
+	scanner := bufio.NewScanner(reader)
 	scanner.Split(rpc.SplicFunc)
 
 	state := analysis.NewState()
-	writer := os.Stdout
+
+	messageHandler := messageHandler.New(reader, writer, state, logger)
 
 	for scanner.Scan() {
 		msg := scanner.Bytes()
@@ -31,149 +33,8 @@ func main() {
 			logger.Printf("Got and error: %s\n", err)
 		}
 
-		handleMessage(logger, writer, state, method, contents)
+		messageHandler.HandleMessage(method, contents)
 	}
-}
-
-func handleMessage(
-	logger *log.Logger,
-	writer io.Writer,
-	state *analysis.State,
-	method string,
-	contents []byte,
-) {
-	logger.Printf("Received %s", method)
-	switch method {
-	case "initialize":
-		var request lsp.InitializeRequest
-		if err := json.Unmarshal(contents, &request); err != nil {
-			logger.Printf("Couldn't parse message (%s) in \"%s\"\n", err, method)
-		}
-		logger.Printf(
-			"Connected to: %s %s",
-			request.Params.ClientInfo.Name,
-			request.Params.ClientInfo.Version,
-		)
-
-		msg := lsp.NewInitializeResponse(request.ID)
-		writeResponse(writer, msg)
-
-		logger.Println("Sent the reply")
-
-	case "textDocument/didOpen":
-		var request lsp.DidOpenTextDocumentNotification
-		if err := json.Unmarshal(contents, &request); err != nil {
-			logger.Printf("Couldn't parse message (%s) in \"%s\"\n", err, method)
-		}
-		logger.Printf(
-			"Opened: %s %s",
-			request.Params.TextDocument.LanguageID,
-			request.Params.TextDocument.URI,
-		)
-
-		diagnostics := state.OpenDocument(
-			request.Params.TextDocument.URI,
-			request.Params.TextDocument.Text,
-		)
-		writeResponse(writer, lsp.PublishDiagnosticsNotification{
-			Notification: lsp.Notification{
-				RPC:    "2.0",
-				Method: "textDocument/publishDiagnostics",
-			},
-			Params: lsp.PublishDiagnosticsParams{
-				URI:         request.Params.TextDocument.URI,
-				Diagnostics: diagnostics,
-			},
-		})
-
-	case "textDocument/didChange":
-		var request lsp.TextDocumentDidChangeNotification
-		if err := json.Unmarshal(contents, &request); err != nil {
-			logger.Printf("Couldn't parse message (%s) in \"%s\"\n", err, method)
-		}
-		logger.Printf(
-			"Updated: %d %s",
-			request.Params.TextDocument.Version,
-			request.Params.TextDocument.URI,
-		)
-
-		for _, change := range request.Params.ContentChanges {
-			diagnostics := state.UpdateDocument(request.Params.TextDocument.URI, change.Text)
-			writeResponse(writer, lsp.PublishDiagnosticsNotification{
-				Notification: lsp.Notification{
-					RPC:    "2.0",
-					Method: "textDocument/publishDiagnostics",
-				},
-				Params: lsp.PublishDiagnosticsParams{
-					URI:         request.Params.TextDocument.URI,
-					Diagnostics: diagnostics,
-				},
-			})
-		}
-
-	case "textDocument/hover":
-		var request lsp.HoverRequest
-		if err := json.Unmarshal(contents, &request); err != nil {
-			logger.Printf("Couldn't parse message (%s) in \"%s\"\n", err, method)
-		}
-		logger.Printf(
-			"Hovered : %d %d %s",
-			request.Params.Position.Line,
-			request.Params.Position.Character,
-			request.Params.TextDocument.URI,
-		)
-
-		response := state.Hover(
-			request.ID,
-			request.Params.TextDocument.URI,
-			request.Params.Position,
-		)
-		writeResponse(writer, response)
-
-	case "textDocument/definition":
-		var request lsp.DefinitionRequest
-		if err := json.Unmarshal(contents, &request); err != nil {
-			logger.Printf("Couldn't parse message (%s) in \"%s\"\n", err, method)
-		}
-		logger.Printf(
-			"Definition: %d %d %s",
-			request.Params.Position.Line,
-			request.Params.Position.Character,
-			request.Params.TextDocument.URI,
-		)
-
-		response := state.Definition(
-			request.ID,
-			request.Params.TextDocument.URI,
-			request.Params.Position,
-		)
-		writeResponse(writer, response)
-
-	case "textDocument/codeAction":
-		var request lsp.CodeActionRequest
-		if err := json.Unmarshal(contents, &request); err != nil {
-			logger.Printf("Couldn't parse message (%s) in \"%s\"\n", err, method)
-		}
-		logger.Printf("Code action: %s", request.Params.TextDocument.URI)
-
-		response := state.TextDocumentCodeAction(request.ID, request.Params.TextDocument.URI)
-		writeResponse(writer, response)
-
-	case "textDocument/completion":
-		var request lsp.CompletionRequest
-		if err := json.Unmarshal(contents, &request); err != nil {
-			logger.Printf("Couldn't parse message (%s) in \"%s\"\n", err, method)
-		}
-		logger.Printf("Completion: %s", request.Params.TextDocument.URI)
-
-		response := state.TextDocumentCompletion(request.ID, request.Params.TextDocument.URI)
-		writeResponse(writer, response)
-	}
-}
-
-func writeResponse(writer io.Writer, msg any) {
-	reply := rpc.EncodeMessage(msg)
-	writer.Write([]byte(reply))
 }
 
 func getLogger(filename string) *log.Logger {
---
+++ internal/messageHandler/messageHandler.go
@@ -0,0 +1,124 @@
+package messageHandler
+
+import (
+	"encoding/json"
+	"io"
+	"log"
+
+	"github.com/marcsek/monkey-language-server/internal/analysis"
+	"github.com/marcsek/monkey-language-server/internal/lsp"
+	"github.com/marcsek/monkey-language-server/internal/rpc"
+)
+
+type MessageHandler struct {
+	reader io.Reader
+	writer io.Writer
+	state  *analysis.State
+	logger *log.Logger
+}
+
+func New(
+	reader io.Reader,
+	writer io.Writer,
+	state *analysis.State,
+	logger *log.Logger,
+) *MessageHandler {
+	return &MessageHandler{
+		reader: reader,
+		writer: writer,
+		state:  state,
+		logger: logger,
+	}
+}
+
+func (mh *MessageHandler) HandleMessage(method string, contents []byte) {
+	mh.logger.Printf("Received %s", method)
+
+	switch method {
+	case "initialize":
+		request := parseMessage[lsp.InitializeRequest](contents, mh.logger, method)
+
+		msg := lsp.NewInitializeResponse(request.ID)
+		mh.sendMessage(msg)
+
+	case "textDocument/didOpen":
+		request := parseMessage[lsp.DidOpenTextDocumentNotification](contents, mh.logger, method)
+
+		diagnostics := mh.state.OpenDocument(
+			request.Params.TextDocument.URI,
+			request.Params.TextDocument.Text,
+		)
+		mh.sendMessage(lsp.PublishDiagnosticsNotification{
+			Notification: lsp.Notification{
+				RPC:    "2.0",
+				Method: "textDocument/publishDiagnostics",
+			},
+			Params: lsp.PublishDiagnosticsParams{
+				URI:         request.Params.TextDocument.URI,
+				Diagnostics: diagnostics,
+			},
+		})
+
+	case "textDocument/didChange":
+		request := parseMessage[lsp.TextDocumentDidChangeNotification](contents, mh.logger, method)
+
+		for _, change := range request.Params.ContentChanges {
+			diagnostics := mh.state.UpdateDocument(request.Params.TextDocument.URI, change.Text)
+			mh.sendMessage(lsp.PublishDiagnosticsNotification{
+				Notification: lsp.Notification{
+					RPC:    "2.0",
+					Method: "textDocument/publishDiagnostics",
+				},
+				Params: lsp.PublishDiagnosticsParams{
+					URI:         request.Params.TextDocument.URI,
+					Diagnostics: diagnostics,
+				},
+			})
+		}
+
+	case "textDocument/hover":
+		request := parseMessage[lsp.HoverRequest](contents, mh.logger, method)
+
+		response := mh.state.Hover(
+			request.ID,
+			request.Params.TextDocument.URI,
+			request.Params.Position,
+		)
+		mh.sendMessage(response)
+
+	case "textDocument/definition":
+		request := parseMessage[lsp.DefinitionRequest](contents, mh.logger, method)
+
+		response := mh.state.Definition(
+			request.ID,
+			request.Params.TextDocument.URI,
+			request.Params.Position,
+		)
+		mh.sendMessage(response)
+
+	case "textDocument/codeAction":
+		request := parseMessage[lsp.CodeActionRequest](contents, mh.logger, method)
+
+		response := mh.state.TextDocumentCodeAction(request.ID, request.Params.TextDocument.URI)
+		mh.sendMessage(response)
+
+	case "textDocument/completion":
+		request := parseMessage[lsp.CompletionRequest](contents, mh.logger, method)
+
+		response := mh.state.TextDocumentCompletion(request.ID, request.Params.TextDocument.URI)
+		mh.sendMessage(response)
+	}
+}
+
+func (mh *MessageHandler) sendMessage(msg any) {
+	reply := rpc.EncodeMessage(msg)
+	mh.writer.Write([]byte(reply))
+}
+
+func parseMessage[T any](contents []byte, logger *log.Logger, method string) T {
+	var request T
+	if err := json.Unmarshal(contents, &request); err != nil {
+		logger.Printf("Couldn't parse message (%s) in \"%s\"\n", err, method)
+	}
+	return request
+}
